5e34480d47359475a668760de3e10930ad45f595
Amanda Morris <amandafmorris@gmail.com>
2017-04-24 00:44:12 +0000

    initial valid_parens commit

---
+++ valid_parens.py
@@ -0,0 +1,12 @@
+"""Description:
+
+Write a function called validParentheses that takes a string of parentheses, and
+determines if the order of the parentheses is valid. validParentheses should
+return true if the string is valid, and false if it's invalid.
+
+Examples: 
+validParentheses( "()" ) => returns true 
+validParentheses( ")(()))" ) => returns false 
+validParentheses( "(" ) => returns false 
+validParentheses( "(())((()())())" ) => returns true 
+"""
