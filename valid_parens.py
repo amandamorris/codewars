64b6af2f082e6b7308d3f727055f082fdaa80e81
Amanda Morris <amandafmorris@gmail.com>
2017-04-24 00:52:08 +0000

    valid_parens done

--- valid_parens.py
+++ valid_parens.py
@@ -10,3 +10,18 @@ validParentheses( ")(()))" ) => returns false
 validParentheses( "(" ) => returns false 
 validParentheses( "(())((()())())" ) => returns true 
 """
+#pseudocode: use a stack, push open parens on the stack, pop from stack for
+#closed parens
+
+def valid_parentheses(string):
+    my_stack = []
+    for char in string:
+        if char == "(":
+            my_stack.append(char)
+            continue
+        elif char == ")":
+            if not my_stack:
+                return False
+            my_stack.pop()
+            continue
+    return my_stack == []
